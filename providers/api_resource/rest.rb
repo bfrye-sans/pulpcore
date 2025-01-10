7d046be0fd66658d664d5e524279d6b1fde570b0
bfrye-sans <48800510+bfrye-sans@users.noreply.github.com>
2025-01-10 18:41:57 -0500

    update

--- providers/api_resource/rest.rb
+++ providers/api_resource/rest.rb
@@ -5,6 +5,9 @@ require 'uri'
 Puppet::Type.type(:api_resource).provide(:rest) do
   desc 'Manage Pulp resources via REST API.'
 
+  # Class-level cache for pulp_href
+  @@pulp_href_cache = {}
+
   def create
     if exists?
       Puppet.info("Resource #{resource[:name]} already exists. Updating instead of creating.")
@@ -23,10 +26,12 @@ Puppet::Type.type(:api_resource).provide(:rest) do
       raise Puppet::Error, "Failed to create #{resource[:category]}: #{response.body}"
     end
 
+    # Cache pulp_href if resource is a publication
     if resource[:category] == 'publication'
-      # Parse and return pulp_href if the resource is a publication
       publication_data = JSON.parse(response.body)
-      Puppet.info("Created publication with pulp_href: #{publication_data['pulp_href']}")
+      pulp_href = publication_data['pulp_href']
+      @@pulp_href_cache[resource[:name]] = pulp_href
+      Puppet.info("Cached pulp_href for publication #{resource[:name]}: #{pulp_href}")
     end
   end
 
@@ -40,6 +45,14 @@ Puppet::Type.type(:api_resource).provide(:rest) do
     response = send_request(uri, req)
     unless response.code.to_i == 200
       raise Puppet::Error, "Failed to update #{resource[:category]}: #{response.body}"
+    end
+
+    # Cache pulp_href if resource is a publication
+    if resource[:category] == 'publication'
+      publication_data = JSON.parse(response.body)
+      pulp_href = publication_data['pulp_href']
+      @@pulp_href_cache[resource[:name]] = pulp_href
+      Puppet.info("Updated cache for pulp_href of publication #{resource[:name]}: #{pulp_href}")
     end
   end
 
@@ -57,6 +70,12 @@ Puppet::Type.type(:api_resource).provide(:rest) do
     unless response.code.to_i == 204
       raise Puppet::Error, "Failed to delete #{resource[:category]}: #{response.body}"
     end
+
+    # Remove from cache if it exists
+    if @@pulp_href_cache.key?(resource[:name])
+      @@pulp_href_cache.delete(resource[:name])
+      Puppet.info("Removed pulp_href for #{resource[:name]} from cache.")
+    end
   end
 
   def exists?
@@ -68,9 +87,33 @@ Puppet::Type.type(:api_resource).provide(:rest) do
     response.code.to_i == 200
   end
 
+  def pulp_href
+    # Return cached pulp_href if available
+    if @@pulp_href_cache.key?(resource[:name])
+      Puppet.info("Using cached pulp_href for #{resource[:name]}")
+      return @@pulp_href_cache[resource[:name]]
+    end
+
+    # Fetch pulp_href from API if not cached
+    Puppet.info("Fetching pulp_href for #{resource[:name]} from API")
+    uri = URI("#{api_base_path}/#{resource[:name]}")
+    req = Net::HTTP::Get.new(uri)
+    set_auth_header(req)
+
+    response = send_request(uri, req)
+    if response.code.to_i == 200
+      publication_data = JSON.parse(response.body)
+      pulp_href = publication_data['pulp_href']
+      @@pulp_href_cache[resource[:name]] = pulp_href
+      Puppet.info("Cached pulp_href for #{resource[:name]}: #{pulp_href}")
+      pulp_href
+    else
+      raise Puppet::Error, "Failed to fetch pulp_href for #{resource[:name]}: #{response.body}"
+    end
+  end
+
   private
 
-  # Determine the base API path dynamically based on repo_type and category
   def api_base_path
     base_paths = {
       'repository' => {
